38e22528a70b1b4255586567e6599366caa0552f
js222xt <js222xt@student.lnu.se>
2014-11-17 11:23:53 +0100

    Test
    Test

---
+++ README.md
@@ -0,0 +1,4 @@
+#Labb 01!
+
+Travis status:
+[![Build Status](https://img.shields.io/travis/js222xt/RIA-Labb01/gh-pages.svg)](https://travis-ci.org/krawaller/algol3)
--- gulpfile.js
+++ gulpfile.js
@@ -13,3 +13,35 @@ gulp.task('copyindex', function() {
     gulp.src('src/index.html')
       .pipe(gulp.dest('dist'));
 });
+
+var jest = require('gulp-jest');
+
+gulp.task('test',function(){
+    return gulp.src('__tests__').pipe(jest({
+      testDirectoryName: "spec",
+      scriptPreprocessor: './support/preprocessor.js',
+      unmockedModulePathPatterns: ['node_modules/react'],
+      testPathIgnorePatterns: [
+        "node_modules",
+        "./support"
+      ]
+    }));
+});
+
+gulp.task('docsindex', function(){
+  folderToc('docs', {
+    name : 'index.html',
+    layout: 'classic',
+    filter: '*.html',
+    title: 'Files'    
+  });
+});
+
+var docco = require('gulp-docco');
+gulp.task('builddocs', function(){
+    gulp.src(['src/*/*.js','src/*.js'])
+      .pipe(docco())
+      .pipe(gulp.dest('./docs'))
+});
+
+gulp.task('docs',['builddocs','docsindex']);
